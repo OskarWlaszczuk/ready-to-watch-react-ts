14d27122b114eea94d2dcde9c3608e6113a58d82
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-08-02 12:38:27 +0200

    Moved index.tsx, GlobalStyles and App component file to 'core' folder

--- src/core/index.tsx
+++ src/core/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from '../reportWebVitals';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './GlobalStyles';
-import { theme } from './theme';
+import { theme } from '../theme';
 import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
