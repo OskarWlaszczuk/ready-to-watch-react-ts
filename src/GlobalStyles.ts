6d9561746663712c5d5b6c50657bfbc211046a20
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-08-02 10:56:50 +0200

    Moved base project styles config to GlobalStyles file, changed project font to Rubik

---
+++ src/GlobalStyles.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    };
+
+    *, ::after, ::before {
+        box-sizing: inherit;
+    };
+
+    body {
+        margin: 0;
+        background-color: ${({ theme }) => theme.colors.white};
+    };
+
+    #root {
+    font-family: "Rubik", sans-serif;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+    };
+`;
