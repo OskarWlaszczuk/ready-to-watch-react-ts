0231205828966ec20c8ba96896e6b9589672769e
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-08-02 11:02:34 +0200

    Remove unused index.css import and adjust App import statement in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './GlobalStyles';
 import { theme } from './theme';
+import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 const queryClient = new QueryClient();
