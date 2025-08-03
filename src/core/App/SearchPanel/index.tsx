c3207118ba6d8b69dd13bae8379bcb4e0dec85ab
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-08-03 11:50:13 +0200

    Add SearchPanel component with styled elements for search functionality

---
+++ src/core/App/SearchPanel/index.tsx
@@ -0,0 +1,9 @@
+import { Search, StyledSearchPanel } from "./styled"
+
+export const SearchPanel = () => {
+    return (
+        <StyledSearchPanel>
+            <Search placeholder="Search for new seans..."></Search>
+        </StyledSearchPanel>
+    )
+}
---
+++ src/core/App/SearchPanel/styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledSearchPanel = styled.aside`
+    display: flex;
+    grid-area: search;
+`;
+
+export const Search = styled.input`
+
+`;
