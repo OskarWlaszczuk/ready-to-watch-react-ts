811516cba6d8029f58f221dc8eeee5030c7ba5f6
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-08-03 15:04:02 +0200

    Moved NavPanel and SearchPanel components files into Layout folder

--- src/core/App/Layout/index.tsx
+++ src/core/App/Layout/index.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { MainContent, StyledLayout } from "./styled";
-import { SearchPanel } from "../SearchPanel";
-import { NavPanel } from "../NavPanel";
+import { SearchPanel } from "./SearchPanel";
+import { NavPanel } from "./NavPanel";
 
 export const Layout = () => {
     return (
