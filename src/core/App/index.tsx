1c862f1189ee3f35a15e12c0465d937ef98744ec
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-08-23 10:35:48 +0200

    Refactor routing in App component to replace Explore with Home and update navigation paths

--- src/core/App/index.tsx
+++ src/core/App/index.tsx
@@ -1,18 +1,23 @@
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Layout } from "./Layout";
-import { Explore } from "../../features/MediaTabs";
+import { Home } from "../../features/home";
 import { Preferences } from "../../features/preferences/Preferences";
+import { Login } from "../../features/auth/Login";
+import { Register } from "../../features/auth/Register";
 
 export const App = () => {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Navigate to="explore/" replace />} />
-          <Route path="explore/" element={<Explore />} />
-          <Route path="preferences/" element={<Preferences />} />
-          <Route path="*" element={<Navigate to="explore/" replace />} />
+          <Route index element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/preferences" element={<Preferences />} />
+          <Route path="*" element={<Navigate to="/home" replace />} />
         </Route>
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/register" element={<Register />} />
+        <Route path="/auth/*" element={<Navigate to="/auth/login" replace />} />
       </Routes>
     </HashRouter>
   );
