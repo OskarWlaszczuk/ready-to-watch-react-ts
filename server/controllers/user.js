bb7836fcf0e0b7d2ade6c5b75482769bc53707f0
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-08-29 18:35:49 +0200

    Changed payload data stored in JWT to nickname

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,8 +2,8 @@ const pool = require("../db");
 
 const getUser = async (request, response) => {
     const user = await pool.query(
-        "SELECT nickname, id FROM users WHERE id = $1",
-        [request.userPayload.id]
+        "SELECT nickname, id FROM users WHERE nickname = $1",
+        [request.userPayload.nickname]
     );
 
     response.json({ user: user.rows[0] });
