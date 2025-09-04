d3c7295aae618601d0727aeec264b0ab11788d77
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-09-04 09:00:37 +0200

    Add cookie-parser middleware and update API route paths

--- server/server.js
+++ server/server.js
@@ -4,13 +4,15 @@ const app = express();
 const path = require("path");
 const authRouter = require("./routes/authRouter");
 const userRouter = require("./routes/userRouter");
+const cookieParser = require("cookie-parser");
 
+app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.use("/auth", authRouter);
-app.use("/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 app.listen(5000, () => {
     console.log("server is listening on port 5000...");
