a4f0aa28b7573b4f46948ced797cbfd59340b299
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-09-06 11:57:27 +0200

    removing token and user data stored in cache to trigger re-render on queries change and update view after user logout

--- src/core/App/Layout/SearchPanel/index.tsx
+++ src/core/App/Layout/SearchPanel/index.tsx
@@ -1,16 +1,23 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import { Search, StyledSearchPanel } from "./styled"
 import { authApiSecure } from "../../../../common/constants/api";
 import { useAccessToken } from "../../../../common/hooks/useAccessToken";
+import { useQueryClient } from "@tanstack/react-query";
 
 export const SearchPanel = () => {
     //@ts-ignore
     const { accessToken } = useAccessToken();
+    const navigate = useNavigate();
 
     //dodać blok try/catch do łapanie błedów z serwera
+    const queryClient = useQueryClient();
     const logout = async () => {
         try {
             await authApiSecure.delete("/logout");
+            queryClient.setQueryData(["secureUser"], null);
+            queryClient.setQueryData(["accessToken"], null);
+
+            navigate("/home"); // lub inna strona po zalogowaniu
         } catch (error) {
 
         }
