03cb021b1497dd6dd04c5c14169f90de6ca74e14
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-09-06 11:53:44 +0200

    Removed useUserInterceptors hook declaration and call, deleted AuthProvider wrapper and its context that was replaced with queries

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,74 +1,5 @@
-import { createContext, useContext, useEffect, useLayoutEffect, useState } from "react";
-import { authApiSecure, userApi } from "../../common/constants/api";
-import { useQuery } from "@tanstack/react-query";
 import { useAccessToken } from "../../common/hooks/useAccessToken";
 import { useUserSecure } from "../../common/hooks/useUserSecure";
-export const AuthContext = createContext(null);
-
-//@ts-ignore
-const useUserInterceptors = (accessToken, refetchAccessToken) => {
-    useLayoutEffect(() => {
-        const userInterceptor = userApi.interceptors.request.use((config) => {
-            if (accessToken) {
-                config.headers.Authorization = `Bearer ${accessToken}`;
-            }
-
-            return config;
-        });
-
-        return () => {
-            userApi.interceptors.request.eject(userInterceptor);
-        };
-    }, [accessToken]);
-
-    const userInterceptor = userApi.interceptors.response.use(
-        response => response,
-        async (error) => {
-            const originalRequest = error.config;
-
-            if (
-                (error.status === 403 || error.status === 401) && !originalRequest._retry
-            ) {
-                originalRequest._retry = true;
-                await refetchAccessToken();
-            }
-
-            //po co tutaj odrzucać obietnicę?
-            //Czy to dlatego, że funkcje async zawsze muszą zwracać obietnice?
-            //Co się stanie po zwróceniu przez tę funkcję niespełnionej obietnicy?
-            return Promise.reject(error);
-        }
-    );
-
-    return () => {
-        userApi.interceptors.response.eject(userInterceptor);
-    };
-};
-
-
-
-//@ts-ignore
-export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
-    const {
-        status,
-        accessToken,
-        isPaused,
-        error,
-        refetchAccessToken
-    } = useAccessToken();
-    // useInitializeAuth(accessToken, setAccessToken);
-    useUserInterceptors(accessToken, refetchAccessToken);
-
-    return (
-        //@ts-ignore
-        <AuthContext.Provider value={{ accessToken, user, setUser }}>
-            {children}
-        </AuthContext.Provider>
-    );
-};
-
-
 
 export const Home = () => {
     const { accessToken } = useAccessToken();
@@ -78,6 +9,7 @@ export const Home = () => {
         status,
         isPaused,
         error,
+        //@ts-ignore
     } = useUserSecure(accessToken);
 
     return (
