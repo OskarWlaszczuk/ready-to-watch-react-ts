5c1260f4723932ca2f1b07c088fadcb58612fc37
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-09-11 08:18:04 +0200

    now getUser fn already returns first el of rows arr returned byt db

--- server/services/getUser.js
+++ server/services/getUser.js
@@ -1,10 +1,12 @@
 const pool = require("../db");
 
-const getUser = async (nickname) => (
-    await pool.query(
+const getUser = async (nickname) => {
+    const user = await pool.query(
         "SELECT * FROM users WHERE nickname = $1",
         [nickname]
-    )
-);
+    );
+
+    return user.rows[0];
+};
 
 module.exports = getUser
