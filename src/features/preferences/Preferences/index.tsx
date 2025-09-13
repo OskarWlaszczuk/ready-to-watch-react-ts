c30cace88c44025effd2b83c8f5b109567ced08e
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-09-13 09:37:21 +0200

    adding and removing preffered genres interaction added

--- src/features/preferences/Preferences/index.tsx
+++ src/features/preferences/Preferences/index.tsx
@@ -8,47 +8,15 @@ import { useUserSecure } from '../../../common/hooks/useUserSecure';
 import { QueryClient, useMutation, useQueryClient } from '@tanstack/react-query';
 import { secureUserApi } from '../../../common/constants/api';
 
-// interface WatchProvider {
-//     display_priority: number;
-//     logo_path: string;
-//     provider_id: number;
-//     provider_name: string;
-// }
-
-// interface ProvidersResponse {
-//     results: WatchProvider[];
-// }
-
-// export const useMovieProviders = (region: string = 'PL', language: string = 'pl-PL') => {
-//     const { data } = useQuery<ProvidersResponse>({
-//         queryKey: ['movieProviders', region, language],
-//         queryFn: () => fetchApi("watch/providers/movie"),
-//         staleTime: 1000 * 60 * 60, // 1 godzina
-//         retry: 1,
-//     });
-
-//     return data;
-// };
-
-export const Preferences = () => {
-    const { genres: movieGenres } = useGenresQuery();
+const useLikeGenreMutation = () => {
     const { accessToken } = useAccessToken();
-
-    const {
-        user: prefferedGenres,
-        status: userGenresStatus,
-        isPaused,
-        error,
-        refetch
-        //@ts-ignore
-    } = useUserSecure({ accessToken, resource: "prefferences/genres" });
     const queryClient = useQueryClient();
 
-    const { status: postStatus, error: errorStatus, mutate: likeGenre } = useMutation({
-        mutationFn: async (tmdb_genre_id) => {
+    const { status: likeGenreStatus, error: likeGenreError, mutate: likeGenre } = useMutation({
+        mutationFn: async (genreId: number) => {
             const response = await secureUserApi.post(
                 "prefferences/genres",
-                { tmdb_genre_id },
+                { genreId },
                 {
                     headers: {
                         Authorization: `Bearer ${accessToken}`
@@ -57,42 +25,103 @@ export const Preferences = () => {
             );
             return response.data;
         },
-        onSuccess: () => {
+        onSuccess: ({ genreId }) => {
             console.log("✅ Genre added successfully");
-            refetch()
-            // queryClient.setQueryData(["prefferences/genres", tmdb_genre_id], tmdb_genre_id)
+            console.log(genreId);
+            queryClient.setQueryData(["secureUser", "prefferences/genres"], ((likedGenres: any) => [
+                ...likedGenres,
+                genreId
+            ]));
+
+
         },
         onError: (error) => {
             console.error("❌ Failed to add genre", error);
         }
     });
 
-    //@ts-ignore
-    // const activeGenres = userGenres?.filter(({ tmdb_genre_id }) => genres?.map(({ id }) => id).includes(tmdb_genre_id));
-    console.log(prefferedGenres);
-    console.log(postStatus);
+    return { likeGenre, likeGenreError, likeGenreStatus };
+};
+
+const useDislikeGenreMutation = () => {
+    const { accessToken } = useAccessToken();
+    const queryClient = useQueryClient();
+
+    const { status: likeGenreStatus, error: likeGenreError, mutate: dislikeGenre } = useMutation({
+        mutationFn: async (genreId: number) => {
+            const response = await secureUserApi.delete(`prefferences/genres/${genreId}`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            });
+
+            return response.data;
+        },
+        onSuccess: ({ genreId }) => {
+            console.log(" Genre removed");
+            console.log(genreId);
+            queryClient.setQueryData(["secureUser", "prefferences/genres"], ((likedGenres: any) => {
+                const updatedGenres = likedGenres.filter(id => id !== Number(genreId));
+                return updatedGenres;
+            }));
+        },
+        onError: (error) => {
+            console.error("❌ Failed to remove genre", error);
+        }
+    });
+
+    return { dislikeGenre, likeGenreError, likeGenreStatus };
+};
+
+
+export const Preferences = () => {
+    const { genres: movieGenres } = useGenresQuery();
+    const { accessToken } = useAccessToken();
+
+    const {
+        user: likedGenres,
+        status: likedGenresStatus,
+        isPaused,
+        error,
+        refetch
+        //@ts-ignore
+    } = useUserSecure({ accessToken, resource: "prefferences/genres" });
+    console.log(likedGenres);
+    const {
+        likeGenre,
+        likeGenreError,
+        likeGenreStatus,
+    } = useLikeGenreMutation();
+
+    const {
+        dislikeGenre
+    } = useDislikeGenreMutation();
     if (!movieGenres) return <></>;
 
-    const isGenreActive = (genre: string) => {
+    const isGenreLiked = (genre: string) => {
         const genreId = movieGenres.find(({ name }) => name === genre)?.id!;
         //@ts-ignore
-        return prefferedGenres.includes(genreId);
-    }
-    const genreListTitle = "Genres";
-    const genreOptions = movieGenres.map(({ name }) => name);
-    const addNewGenre = (genre: string) => {
-        const genreId = movieGenres.find(({ name }) => name === genre)?.id!;
-        //@ts-ignore
-        likeGenre(genreId);
+        return likedGenres.includes(genreId);
     };
 
-    if (userGenresStatus === "pending") return <>Loadding...</>
+    const genreListTitle = "Genres";
+    const genreOptions = movieGenres.map(({ name }) => name);
+
+    const onGenreClick = (genre: string) => {
+        const genreId = movieGenres.find(({ name }) => name === genre)?.id!;
+
+        isGenreLiked(genre) ?
+            dislikeGenre(genreId) :
+            likeGenre(genreId)
+    };
+
+    if (likedGenresStatus === "pending") return <>Loadding...</>
 
     return (
         <>
             <MultiSelectList
-                onOptionChange={addNewGenre}
-                isOptionActive={isGenreActive}
+                onOptionChange={onGenreClick}
+                isOptionActive={isGenreLiked}
                 title={genreListTitle}
                 options={genreOptions}
             />
