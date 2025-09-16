8b31e5b09ee4fabafca367cba91a2aedc2aebb57
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-09-16 12:09:45 +0200

    Implement genre preference management: add, retrieve, and delete preferred genres for users

--- server/routes/secureUserRouter.js
+++ server/routes/secureUserRouter.js
@@ -4,7 +4,7 @@ const secureUserRouter = express.Router();
 const jwt = require("jsonwebtoken");
 const { getUserNickname, getUserAccontDate } = require("../controllers/secureUser");
 const util = require("util");
-const getUser = require("../services/getUser");
+const pool = require("../db");
 const verifyJwt = util.promisify(jwt.verify);
 
 const authenticateToken = async (request, response, next) => {
@@ -22,8 +22,8 @@ const authenticateToken = async (request, response, next) => {
     }
 
     try {
-        const payload = await verifyJwt(accessToken, process.env.ACCESS_TOKEN_SECRET);
-        request.payload = payload;
+        const tokenPayload = await verifyJwt(accessToken, process.env.ACCESS_TOKEN_SECRET);
+        request.tokenPayload = tokenPayload;
         next();
     } catch (error) {
         if (error.name === "TokenExpiredError") {
@@ -34,19 +34,96 @@ const authenticateToken = async (request, response, next) => {
 
 const isUserExists = async (request, response, next) => {
     console.log('Checking is user exists...');
-    const { payload } = request;
+    const { tokenPayload } = request;
 
-    const result = await getUser(payload.nickname);
+    const result = await pool.query(
+        "SELECT * FROM users WHERE id = $1",
+        [tokenPayload.id]
+    );
+
     if (result.rows.length === 0) {
         return response
             .status(400)
             .json({ success: false, message: "User doesn't exist" });
     }
-
+    console.log(`User exists`);
     next();
+};
+
+const likeGenre = async (request, response) => {
+    console.log(`Adding preffered genre...`);
+
+    const {  genreId } = request.body;
+    const { tokenPayload } = request;
+
+    try {
+        await pool.query(
+            // ON CONFLICT DO NOTHING, jak działa
+            "INSERT INTO preferred_genres_by_user (user_id, tmdb_genre_id) VALUES ($1, $2) ON CONFLICT DO NOTHING",
+            [tokenPayload.id, genreId]
+        );
+        response.status(201).json({ genreId });
+    } catch (err) {
+        console.error(err);
+        response.status(500).json({ error: "Database error" });
+    }
+};
+
+const getLikedGenres = async (request, response) => {
+    const { tokenPayload } = request;
+
+    try {
+        const result = await pool.query(
+            "SELECT tmdb_genre_id FROM preferred_genres_by_user WHERE user_id = $1",
+            [tokenPayload.id]
+        );
+        const tmdbGenresIds = result.rows.map(({ tmdb_genre_id }) => tmdb_genre_id);
+        console.log(tmdbGenresIds);
+
+        response.json({
+            success: true,
+            data: tmdbGenresIds,
+        });
+    } catch (err) {
+        console.error(err);
+        response.status(500).json({
+            success: false,
+            message: 'error',
+        });
+    }
+};
+
+const dislikeGenre = async (request, response) => {
+    console.log(`Disliking genre...`);
+    
+    const { tokenPayload } = request;
+    const { genreId } = request.params;
+
+    try {
+        await pool.query(
+            "DELETE FROM preferred_genres_by_user WHERE tmdb_genre_id = $1 AND user_id = $2",
+            [genreId, tokenPayload.id]
+        );
+
+        response
+            .status(200)
+            .json({
+                success: true,
+                genreId,
+            });
+    } catch (err) {
+        console.error(err);
+        response.status(500).json({
+            success: false,
+            message: 'error',
+        });
+    }
 };
 
 secureUserRouter.route("/nickname").get(authenticateToken, isUserExists, getUserNickname);
 secureUserRouter.route("/accountDate").get(authenticateToken, isUserExists, getUserAccontDate);
+secureUserRouter.route("/prefferences/genres").post(authenticateToken, isUserExists, likeGenre);
+secureUserRouter.route("/prefferences/genres").get(authenticateToken, isUserExists, getLikedGenres);
+secureUserRouter.route("/prefferences/genres/:genreId").delete(authenticateToken, isUserExists, dislikeGenre);
 
 module.exports = secureUserRouter;
