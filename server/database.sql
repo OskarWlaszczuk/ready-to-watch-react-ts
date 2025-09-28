b93a67b6faeb476d7e3a069c49723489bb304fa3
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-09-28 11:48:00 +0200

    Added crews_roles entity table for storing roles like 'director', 'scriptwritter' etc and theirs id. This table is important for storing people by their movie crew role.

--- server/database.sql
+++ server/database.sql
@@ -1,67 +1,103 @@
-CREATE DATABASE usersdb;
+CREATE
+DATABASE usersdb;
 
-CREATE TABLE users(
-    id SERIAL PRIMARY KEY,      
-    nickname VARCHAR(50) NOT NULL,
-    password TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
+CREATE TABLE users
+(
+    id                 SERIAL PRIMARY KEY,
+    nickname           VARCHAR(50) NOT NULL,
+    password           TEXT        NOT NULL,
+    created_at         TIMESTAMP DEFAULT NOW(),
     refresh_token_hash VARCHAR(350)
 );
 
-CREATE TABLE genres (
-  id INT PRIMARY KEY,
-  name TEXT UNIQUE NOT NULL
+CREATE TABLE genres
+(
+    id   INT PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL
 );
 
-CREATE TABLE liked_genres_by_users (
-  user_id INT NOT NULL,
-  genre_id INT NOT NULL,
-  created_at TIMESTAMP DEFAULT NOW(),
-  PRIMARY KEY (user_id, genre_id),
-  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-  FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE
+CREATE TABLE liked_genres_by_users
+(
+    user_id    INT NOT NULL,
+    genre_id   INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    PRIMARY KEY (user_id, genre_id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (genre_id) REFERENCES genres (id) ON DELETE CASCADE
 );
 
-CREATE TABLE collections (
-  id INT PRIMARY KEY,
-  name TEXT UNIQUE NOT NULL
+CREATE TABLE collections
+(
+    id     INT PRIMARY KEY,
+    name   TEXT UNIQUE NOT NULL,
+    poster VARCHAR(100) UNIQUE
 );
 
-CREATE TABLE liked_collections_by_users (
-  user_id INT NOT NULL,
-  collection_id INT NOT NULL,
-  created_at TIMESTAMP DEFAULT NOW(),
-  PRIMARY KEY (user_id, collection_id),
-  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-  FOREIGN KEY (collection_id) REFERENCES collections(id) ON DELETE CASCADE
+CREATE TABLE liked_collections_by_users
+(
+    user_id       INT NOT NULL,
+    collection_id INT NOT NULL,
+    created_at    TIMESTAMP DEFAULT NOW(),
+    PRIMARY KEY (user_id, collection_id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (collection_id) REFERENCES collections (id) ON DELETE CASCADE
 );
 
-CREATE TABLE studios (
-  id INT PRIMARY KEY,
-  name TEXT UNIQUE NOT NULL
+CREATE TABLE studios
+(
+    id   INT PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,
+    logo VARCHAR(100) UNIQUE
 );
 
-CREATE TABLE liked_studios_by_users (
-  user_id INT NOT NULL,
-  studio_id INT NOT NULL,
-  created_at TIMESTAMP DEFAULT NOW(),
-  PRIMARY KEY (user_id, studio_id),
-  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-  FOREIGN KEY (studio_id) REFERENCES studios(id) ON DELETE CASCADE
+CREATE TABLE liked_studios_by_users
+(
+    user_id    INT NOT NULL,
+    studio_id  INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    PRIMARY KEY (user_id, studio_id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (studio_id) REFERENCES studios (id) ON DELETE CASCADE
 );
 
-CREATE TABLE runtime_categories (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100) UNIQUE NOT NULL,       
-  min_minutes INT NOT NULL,              
-  max_minutes INT                          
+CREATE TABLE watch_providers
+(
+    id   INT PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,
+    logo VARCHAR(100) UNIQUE,
 );
 
-CREATE TABLE liked_runtime_categories_by_users (
-  user_id INT NOT NULL,
-  runtime_category_id INT NOT NULL,
-  created_at TIMESTAMP DEFAULT NOW(),
-  PRIMARY KEY (user_id, runtime_category_id),
-  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-  FOREIGN KEY (runtime_category_id) REFERENCES runtime_categories(id) ON DELETE CASCADE
+CREATE TABLE liked_watch_providers_by_users
+(
+    user_id           INT NOT NULL,
+    watch_provider_id INT NOT NULL,
+    created_at        TIMESTAMP DEFAULT NOW(),
+    PRIMARY KEY (user_id, watch_provider_id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (watch_provider_id) REFERENCES watch_providers (id) ON DELETE CASCADE
 );
+
+CREATE TABLE runtime_categories
+(
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(100) UNIQUE NOT NULL,
+    min_minutes INT                 NOT NULL,
+    max_minutes INT
+);
+
+CREATE TABLE liked_runtime_categories_by_users
+(
+    user_id             INT NOT NULL,
+    runtime_category_id INT NOT NULL,
+    created_at          TIMESTAMP DEFAULT NOW(),
+    PRIMARY KEY (user_id, runtime_category_id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (runtime_category_id) REFERENCES runtime_categories (id) ON DELETE CASCADE
+);
+
+CREATE TABLE crews_roles
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL
+);
+
